38e1f99a3f043fc1b8685c31ca955db5bf573fc1
 MacBook <macbook@MacBook-Pro-MacBook.local>
2023-12-09 15:20:36 +0500

    initial commit

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,142 @@
+const token = JSON.parse(localStorage.getItem("token") || "false");
+setTimeout(() => {
+    if (!token) {
+        window.location.pathname = "login.html"
+    }
+}, 4000);
+const template = document.querySelector(".product_template").content;
+const fragment = new DocumentFragment();
 const form = document.querySelector(".form");
+const productName = document.querySelector(".name");
+const productDesc = document.querySelector(".desc");
+const productImg = document.querySelector(".img");
+const productPrice = document.querySelector(".price");
+const List = document.querySelector(".list");
+const Item = document.querySelector(".item");
+/*  edit modal form */
+const editForm = document.querySelector(".edit-form");
+const editName = document.querySelector(".edit-name");
+const editDesc = document.querySelector(".edit-desc");
+const editImage = document.querySelector(".edit-img");
+const editPrice = document.querySelector(".edit-price");
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.set("product_name", productName.value)
+    formData.set("product_img", productImg.files[0])
+    formData.set("product_desc", productDesc.value)
+    formData.set("product_price", productPrice.value)
+    await adminPost(formData);
+})
+
+
+
+/*  adminpost */
+async function adminPost(data) {
+    try {
+        const respons = await fetch("http://localhost:5000/product", {
+            method: "POST",
+            headers: {
+                Authorization: token.token,
+            },
+            body: data,
+        });
+        const result = await respons.json();
+        window.location.reload();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function renderProduct(product_data) {
+    product_data.forEach(element => {
+        const tempalteClone = template.cloneNode(true);
+        tempalteClone.querySelector(".image").src = `http://localhost:5000/${element.product_img}`;
+        tempalteClone.querySelector(".product-name").textContent = element.product_name;
+        tempalteClone.querySelector(".product-price").textContent = element.product_price;
+        tempalteClone.querySelector(".product-desc").textContent = element.product_desc;
+        const deletebtn = tempalteClone.querySelector(".delete");
+        const editBtn = tempalteClone.querySelector(".edit");
+        deletebtn.setAttribute("data-id", `${element.id}`);
+        editBtn.setAttribute("data-id", `${element.id}`);
+        fragment.append(tempalteClone);
+    });
+    List.append(fragment);
+}
+/*  admin get */
+async function adminGet() {
+    try {
+        const getrespons = await fetch("http://localhost:5000/product", {
+            method: "GET",
+            headers: {
+                Authorization: token.token,
+            }
+        });
+        const getdata = await getrespons.json();
+        renderProduct(getdata)
+    } catch (error) {
+        console.log(error)
+    }
+}
+adminGet()
+
+/*  admindelete */
+async function adminDelete(id) {
+    try {
+        const deleteRespons = await fetch(`http://localhost:5000/product/` + id, {
+            method: "DELETE",
+            headers: {
+                Authorization: token.token,
+            },
+        });
+        const deleteResult = await deleteRespons.json();
+        window.location.reload()
+        renderProduct(product_data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+List.addEventListener("click", async (e) => {
+    if (e.target.matches(".delete")) {
+        const id = e.target.dataset.id;
+        await adminDelete(id);
+        adminGet()
+    }
+})
+
+
+
+/*  edit form */
+List.addEventListener("click", async (e) => {
+    if (e.target.matches(".edit")) {
+        const editId = e.target.dataset.id;
+        localStorage.setItem("id", JSON.stringify(editId));
+    }
+})
+editForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const editId = JSON.parse(localStorage.getItem("id"))
+    await adminEdit(editId)
+    window.location.reload();
+});
+async function adminEdit(id) {
+    try {
+        const editFormData = new FormData();
+        editFormData.set("product_name", editName.value);
+        editFormData.set("product_desc", editDesc.value);
+        editFormData.set("product_img", editImage.files[0]);
+        editFormData.set("product_price", editPrice.value)
+        const editRespons = await fetch(`http://localhost:5000/product/` + id, {
+            method: "PUT",
+            headers: {
+                Authorization: token.token,
+            },
+            body: editFormData,
+        });
+        const editresult = await editRespons.json();
+        console.log(editresult)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
--- js/app.js
+++ js/app.js
@@ -1,11 +1,139 @@
-// const productTemplate = document.querySelector(".product__tempalte").content;
-const token = JSON.parse(localStorage.getItem("token") || "false")
-console.log(token)
-const buyButton = document.querySelector(".sale__buy");
-buyButton.addEventListener("click", (e) => {
-    e.preventDefault();
+const token = JSON.parse(localStorage.getItem("token") || "false");
+setTimeout((item) => {
     if (!token) {
-        console.log("salom")
-        window.location.href = "login.html"
+        document.body.classList.add("fixed");
+        document.querySelector(".sec-modal").classList.add("sec-flex-modal");
+    }
+    else {
+        document.body.classList.remove("fixed");
+        document.querySelector(".sec-modal").classList.remove("sec-flex-modal");
+    }
+}, 5000);
+const productTemplate = document.querySelector(".product__tempalte").content;
+const fragment = new DocumentFragment();
+const list = document.querySelector(".sale__list");
+const buyButton = document.querySelector(".sale__buy");
+const orderList = document.querySelector(".order__list");
+const orderTemplate = document.querySelector(".order__tempalte").content;
+const orderFragment = new DocumentFragment();
+function renderMain(data) {
+    list.innerHTML = ""
+    data.forEach(element => {
+        const templateClone = productTemplate.cloneNode(true);
+        templateClone.querySelector(".sale__image").src = `http://localhost:5000/${element.product_img}`;
+        templateClone.querySelector(".sale__product-name").textContent = element.product_name;
+        templateClone.querySelector(".sale__product-desc").textContent = element.product_desc;
+        templateClone.querySelector(".sale__product-price").textContent = element.product_price;
+        const BuyBtn = templateClone.querySelector(".sale__buy");
+        BuyBtn.setAttribute("data-id", element.id);
+        fragment.append(templateClone);
+    });
+    list.append(fragment);
+}
+
+async function get() {
+    try {
+        const getRespons = await fetch("http://localhost:5000/product", {
+            method: "GET",
+            headers: {
+                Authorization: token.token,
+            },
+        });
+        const getResult = await getRespons.json();
+        renderMain(getResult)
+    } catch (error) {
+        console.log(error)
+    }
+}
+get()
+
+/* post orders */
+
+list.addEventListener("click", async (e) => {
+    if (e.target.matches(".sale__buy")) {
+        if (token) {
+            const buyId = e.target.dataset.id;
+            await orderPost(buyId);
+            await orderGet()
+        }
+        else {
+            window.location.pathname = "login.html"
+        }
+    }
+
+})
+
+function orderRender(arr) {
+    orderList.innerHTML = ""
+    arr.forEach(item => {
+        const orderTemplateClone = orderTemplate.cloneNode(true);
+        orderTemplateClone.querySelector(".order__image").src = `http://localhost:5000/${item.product_img}`;
+        orderTemplateClone.querySelector(".order__name").textContent = `${item.product_name}`;
+        orderTemplateClone.querySelector(".username").textContent = `${item.user_name}`;
+        const removeBtn = orderTemplateClone.querySelector(".order__remove");
+        removeBtn.setAttribute("data-id", `${item.id}`);
+        orderFragment.append(orderTemplateClone);
+    })
+    orderList.append(orderFragment);
+}
+
+
+async function orderPost(id) {
+    const orderRespons = await fetch("http://localhost:5000/order",
+        {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Authorization: token.token,
+            },
+            body: JSON.stringify({
+                product_id: Number(id)
+            }),
+        })
+    const orderResult = await orderRespons.json();
+
+}
+
+
+orderList.addEventListener("click", async (e) => {
+    if (e.target.matches(".order__remove")) {
+        const orderID = e.target.dataset.id;
+        await orderDelete(orderID);
+        await orderGet()
     }
 })
+
+
+async function orderGet() {
+    const orderGetRespons = await fetch("http://localhost:5000/order", {
+        method: "GET",
+        headers: {
+            Authorization: token.token
+        }
+    });
+    const orderGetResult = await orderGetRespons.json();
+    document.querySelector(".position-absolute").textContent = orderGetResult.length;
+    orderRender(orderGetResult)
+
+}
+orderGet()
+
+/*   orderDelete*/
+async function orderDelete(id) {
+    try {
+        const orderDeleteRes = await fetch(`http://localhost:5000/order/` + id, {
+            method: "DELETE",
+            headers: {
+                Authorization: token.token
+            },
+        });
+        const orderDeleteResult = await orderDeleteRes.json();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
